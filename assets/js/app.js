a74a14bd28c3a54cf0b468067b7a90f9d3fbc092
Atsushi <atsushi88885@gmail.com>
2019-03-07 00:10:50 +0900

    ボタンを押すたび結果変わるよう修正

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,9 @@
 let res = ['大吉', '吉', '中吉', '凶', '大凶']; //結果の配列
 
-let resLen = res.length - 1; //結果の配列の要素数 -1 ※先頭の添字は0のため
-let randomNum = Math.random(); //0 〜 1までのランダムな数
-
-let num = Math.round(randomNum * resLen); //randomNum * resLenの結果を四捨五入
-console.log(res[num]); 
-
 document.getElementById('btn').addEventListener('click', function() {
-  console.log(1);
+  let resLen = res.length - 1; //結果の配列の要素数 -1 ※先頭の添字は0のため
+  let randomNum = Math.random(); //0 〜 1までのランダムな数
+  let num = Math.round(randomNum * resLen); //randomNum * resLenの結果を四捨五入
+  console.log(res[num]); 
 });
 
