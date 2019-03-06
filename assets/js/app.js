ebe738407cd3b7477526c174a072af0488d6f789
Atsushi <atsushi88885@gmail.com>
2019-03-07 00:14:27 +0900

    画面に表示

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ document.getElementById('btn').addEventListener('click', function() {
   let resLen = res.length - 1; //結果の配列の要素数 -1 ※先頭の添字は0のため
   let randomNum = Math.random(); //0 〜 1までのランダムな数
   let num = Math.round(randomNum * resLen); //randomNum * resLenの結果を四捨五入
-  console.log(res[num]); 
+
+  document.getElementById('result').textContent = res[num];
 });
 
