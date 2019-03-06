d25b0080c997eea5e12d3c6e5b13bfc725acc6a5
Atsushi <atsushi88885@gmail.com>
2019-03-07 00:31:24 +0900

    処理を関数化

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
-let res = ['大吉', '吉', '中吉', '凶', '大凶']; //結果の配列
+document.getElementById('btn').addEventListener('click', omikuji);
 
-document.getElementById('btn').addEventListener('click', function() {
+function omikuji() {
+  let res = ['大吉', '吉', '中吉', '凶', '大凶']; //結果の配列
+
   let resLen = res.length - 1; //結果の配列の要素数 -1 ※先頭の添字は0のため
   let randomNum = Math.random(); //0 〜 1までのランダムな数
   let num = Math.round(randomNum * resLen); //randomNum * resLenの結果を四捨五入
 
   document.getElementById('result').textContent = res[num];
-});
-
+}
